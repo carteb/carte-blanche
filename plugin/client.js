df6a30b9938e031e759a1087edad31c83b8cc3e1
Nik Graf <nik@nikgraf.com>
2016-02-02 22:12:41 -0800

    test es2015 features

--- plugin/client.js
+++ plugin/client.js
@@ -10,12 +10,26 @@
 
 console.log('Works');
 
-var printMore = require('./test');
+const printMore = require('./test');
 // import printMore from './test';
 printMore();
 
-var React = require('react');
+const React = require('react');
 console.log(React);
 
-var cube = require('./cube.gif');
+const cube = require('./cube.gif');
 console.log(cube);
+
+class G {
+  constructor(props) {
+    // super(props);
+    console.log('constructor');
+  }
+
+  render() {
+    return 'some text';
+  }
+}
+
+const a = new G();
+console.log(a.render());
