2d04b2d4b03dc32c329b2a760a02f2286e14b19c
Nik Graf <nik@nikgraf.com>
2016-02-15 11:52:11 -0800

    feat(playground): add integer control

---
+++ client/components/Playground/IntegerControl/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import RandomButton from './RandomButton';
+import valueOrNullOrUndefined from './valueOrNullOrUndefined';
+
+const IntegerControl = (props) => {
+  const { label, value, onUpdate } = props;
+  return (
+    <div>
+      <label>
+        {label}
+        <input
+          type="number"
+          step="1"
+          value={value}
+          onChange={(event) => onUpdate({ value: parseInt(event.target.value, 10) })}
+        />
+      </label>
+      <RandomButton onClick={ () => onUpdate({ value: IntegerControl.randomValue(props) }) }/>
+      {typeof value === 'undefined' ? 'undefined' : null}
+      {value === null ? 'null' : null}
+    </div>
+  );
+};
+
+IntegerControl.randomValue = ({ random = {} }) => {
+  const {
+    canBeNull = true,
+    canBeUndefined = true,
+    min = Number.MAX_SAFE_INTEGER,
+    max = Number.MIN_SAFE_INTEGER,
+    step = 1,
+  } = random;
+  let number;
+  do {
+    number = Math.floor(Math.random() * (max - min + 1)) + min;
+  } while (number % step === 1);
+
+  return valueOrNullOrUndefined(number, canBeNull, canBeUndefined);
+};
+
+export default IntegerControl;
