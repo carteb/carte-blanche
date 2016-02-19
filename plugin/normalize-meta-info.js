4b76d884f4dca8aaec49ed3d93d69d3d21a47c5a
Nik Graf <nik@nikgraf.com>
2016-02-19 13:02:43 -0800

    fix edge case where the property is name type

--- plugin/normalize-meta-info.js
+++ plugin/normalize-meta-info.js
@@ -3,22 +3,28 @@ const has = require('lodash/has');
 const omit = require('lodash/omit');
 
 const normalizeMetaInfo = (metaInformation) => {
-  if (has(metaInformation, 'flowType')) {
-    const newObject = {
+  // there can be the case where a property is `flowType` and therefor we need to
+  // check if flowType contains name
+  const hasFlowType = (has(metaInformation, 'flowType') && has(metaInformation, ['flowType', 'name']));
+  if (hasFlowType) {
+    const info = {
       ...metaInformation,
       name: metaInformation.flowType.name,
       value: metaInformation.flowType.value,
     };
-    return omit(newObject, 'flowType');
+    return omit(info, 'flowType');
   }
 
-  if (has(metaInformation, 'type')) {
-    const newObject = {
+  // there can be the case where a property is `type` and therefor we need to
+  // check if type contains name
+  const hasType = (has(metaInformation, 'type') && has(metaInformation, ['type', 'name']));
+  if (hasType) {
+    const info = {
       ...metaInformation,
       name: metaInformation.type.name,
       value: metaInformation.type.value,
     };
-    return omit(newObject, 'type');
+    return omit(info, 'type');
   }
 
   if (typeof metaInformation === 'object') {
