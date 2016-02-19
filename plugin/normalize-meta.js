924d376f6d3ac4da6b723463b4d805dd66837446
Max <contact@mstoiber.com>
2016-02-18 17:31:34 -0800

    Fix arrays being converted to objects
    typeof new Array() is 'object', so the check if the thing was an array never hit

--- plugin/normalize-meta.js
+++ plugin/normalize-meta.js
@@ -12,7 +12,13 @@ function normalize(metaInformation) {
   const normalized = {};
 
   forIn(metaInformation, function cb(key) {
-    if (typeof metaInformation[key] === 'object') {
+    if (Array.isArray(metaInformation[key])) {
+      // Normalize nested arrays
+      normalized[key] = [];
+      for (let i = 0; i < metaInformation[key].length; i++) {
+        normalized[key][i] = normalize(metaInformation[key][i]);
+      }
+    } else if (typeof metaInformation[key] === 'object') {
       // Normalize nested objects
       if (key === 'flowType') {
         // Replace 'flowType' with 'type'
@@ -22,11 +28,6 @@ function normalize(metaInformation) {
 
       normalized[key] = normalize(metaInformation[key]);
       return;
-    } else if (Array.isArray(metaInformation[key])) {
-      // Normalize nested arrays
-      for (let i = 0; i < metaInformation[key].length; i++) {
-        normalized[key][i] = normalize(metaInformation[key][i]);
-      }
     }
 
     normalized[key] = metaInformation[key];
