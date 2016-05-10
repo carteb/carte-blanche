a3e888e40a860115b92c284e5fbfca0e2adcd2e0
Nik Graf <nik@nikgraf.com>
2016-05-10 18:30:54 +0200

    fix react docs & source plugin

--- plugin/styleguide-plugins/react-docs/component.js
+++ plugin/styleguide-plugins/react-docs/component.js
@@ -1,3 +1,5 @@
-export default function reactDocsFrontend(options, data, Component, React) {
+import React from 'react';
+
+export default function reactDocsFrontend(options) {
   return (<div>{JSON.stringify(options.reactDocs)}</div>);
 }
