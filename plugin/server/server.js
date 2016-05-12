8945fa22035299e193c7f187e68e1f0ba902693c
Max <contact@mstoiber.com>
2016-05-12 09:37:53 +0200

    Properly bail out early if no file exists
    Previously, if a file exists we returned an empty object but still ran
    the remaining code. By adding this return, we bail out and the remaining
    code does not run. (the way it should be)

--- plugin/server/server.js
+++ plugin/server/server.js
@@ -4,6 +4,12 @@ var path = require('path');
 var fs = require('fs');
 var server;
 
+/**
+ * Checks if a file at a certain path exists
+ *
+ * @param  {string} path
+ * @return {bool}
+ */
 var fileExists = (path) => {
   try {
     fs.accessSync(path, fs.R_OK && fs.W_OK);
@@ -16,10 +22,16 @@ var fileExists = (path) => {
 var start = (componentBasePath, variationsBasePath) => {
   var app = express();
 
+  /**
+   * GET /*
+   */
   app.get('/*', (req, res) => {
+    // Get the path of the component from the base path and the passed in parameter
     var componentPath = path.join(componentBasePath, req.params[0]);
+    // If no file exists at the component path, bail out early
     if (fileExists(componentPath) === false) {
       res.json({ data: {} });
+      return;
     }
 
     var variationsPath = path.join(variationsBasePath, req.params[0].replace('.js', ''));
@@ -33,6 +45,9 @@ var start = (componentBasePath, variationsBasePath) => {
     res.json({ data: variations });
   });
 
+  /**
+   * DELETE /*
+   */
   app.delete('/*', (req, res) => {
     var componentPath = path.join(componentBasePath, req.params[0]);
     if (fileExists(componentPath) === false) {
