8f0d5a59a38c7e95d2f2a9b3f2f203b6b3ba0462
Nik Graf <nik@nikgraf.com>
2016-05-14 15:37:59 +0200

    refactor client

--- client/client.js
+++ client/client.js
@@ -11,27 +11,19 @@ import {
 } from 'react-router';
 import ComponentPreview from './components/ComponentPreview';
 import App from './components/App';
+import mapValues from 'lodash/mapValues';
+import values from 'lodash/values';
+
 const styleguideClientApi = window.STYLEGUIDE_PLUGIN_CLIENT_API;
 
 // Generate a view for every component
-const routeViews = {};
-Object.keys(styleguideClientApi.scripts).forEach((componentPath) => {
-  routeViews[componentPath] = () => (
-    <ComponentPreview
-      path={componentPath}
-    />
-  );
-});
-
-// Generate a route for every view
-const routes = Object.keys(routeViews)
-    .map((componentPath) => (
-      <Route
-        key={componentPath}
-        path={componentPath}
-        component={routeViews[componentPath]}
-      />
-    ));
+const routes = values(mapValues(styleguideClientApi.scripts, (value, componentPath) => (
+  <Route
+    key={componentPath}
+    path={componentPath}
+    component={() => <ComponentPreview path={componentPath} />}
+  />
+)));
 
 ReactDOM.render(
   <Router history={hashHistory}>
