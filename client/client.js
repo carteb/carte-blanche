c6424d199d7bc752d6117c2b69ec2a5fa602496a
Nik Graf <nik@nikgraf.com>
2016-05-17 10:55:39 +0200

    replace values(mapValues with map

--- client/client.js
+++ client/client.js
@@ -11,19 +11,18 @@ import {
 } from 'react-router';
 import ComponentPreview from './components/ComponentPreview';
 import App from './components/App';
-import mapValues from 'lodash/mapValues';
-import values from 'lodash/values';
+import map from 'lodash/map';
 
 const styleguideClientApi = window.STYLEGUIDE_PLUGIN_CLIENT_API;
 
 // Generate a view for every component
-const routes = values(mapValues(styleguideClientApi.scripts, (value, componentPath) => (
+const routes = map(styleguideClientApi.scripts, (value, componentPath) => (
   <Route
     key={componentPath}
     path={componentPath}
     component={() => <ComponentPreview path={componentPath} />}
   />
-)));
+));
 
 ReactDOM.render(
   <Router history={hashHistory}>
