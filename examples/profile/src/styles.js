1037bb6c5002a5415e28e9caef23fb21fc1cdbbb
Max <contact@mstoiber.com>
2016-05-20 19:12:46 +0200

    Add new example for the demo

---
+++ examples/profile/src/styles.js
@@ -0,0 +1,11 @@
+const styles = {
+  wrapper: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    height: '100%',
+  },
+};
+
+export default styles;
