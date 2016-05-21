f9806f04a2e2ad27d08a4502f638eec901854035
Nik Graf <nik@nikgraf.com>
2016-05-21 21:14:39 +0200

    refactoring

--- client/components/Navigation/smoothscroll.js
+++ client/components/Navigation/smoothscroll.js
@@ -1,64 +1,61 @@
-/* eslint-disable */
 // based on https://github.com/alicelieutier/smoothScroll
 
-var getTop = function(element) {
-    // return value of html.getBoundingClientRect().top ... IE : 0, other browsers : -pageYOffset
-    if(element.nodeName === 'HTML') return -window.pageYOffset
-    return element.getBoundingClientRect().top + window.pageYOffset;
-}
+const getTop = (element) => {
+  // return value of html.getBoundingClientRect().top ... IE : 0, other browsers : -pageYOffset
+  if (element.nodeName === 'HTML') return -window.pageYOffset;
+  return element.getBoundingClientRect().top + window.pageYOffset;
+};
+
 // ease in out function thanks to:
 // http://blog.greweb.fr/2012/02/bezier-curve-based-easing-functions-from-concept-to-implementation/
-var easeInOutCubic = function (t) { return t<.5 ? 4*t*t*t : (t-1)*(2*t-2)*(2*t-2)+1 }
+const easeInOutCubic = (t) => (
+  t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1
+);
 
 // calculate the scroll position we should be in
 // given the start and end point of the scroll
 // the time elapsed from the beginning of the scroll
 // and the total duration of the scroll (default 500ms)
-var position = function(start, end, elapsed, duration) {
-    if (elapsed > duration) return end;
-    return start + (end - start) * easeInOutCubic(elapsed / duration); // <-- you can change the easing funtion there
-    // return start + (end - start) * (elapsed / duration); // <-- this would give a linear scroll
-}
+const position = (start, end, elapsed, duration) => {
+  if (elapsed > duration) return end;
+  return start + (end - start) * easeInOutCubic(elapsed / duration);
+};
 
 // we use requestAnimationFrame to be called by the browser before every repaint
 // if the first argument is an element then scroll to the top of this element
 // if the first argument is numeric then scroll to this location
 // if the callback exist, it is called when the scrolling is finished
 // if context is set then scroll that element, else scroll window
-var smoothScroll = function(el, duration, callback, context){
-    duration = duration || 500;
-    context = context || window;
-    var start = window.pageYOffset;
+const smoothScroll = (el, durationParam, callback) => {
+  const duration = durationParam || 500;
+  const start = window.pageYOffset;
+  let end;
 
-    if (typeof el === 'number') {
-      var end = parseInt(el);
+  if (typeof el === 'number') {
+    end = parseInt(el, 10);
+  } else {
+    end = getTop(el);
+  }
+
+  const clock = Date.now();
+  const requestAnimationFrame = window.requestAnimationFrame ||
+      window.mozRequestAnimationFrame ||
+      window.webkitRequestAnimationFrame ||
+      ((fn) => { window.setTimeout(fn, 15); });
+
+  const step = () => {
+    const elapsed = Date.now() - clock;
+    window.scroll(0, position(start, end, elapsed, duration));
+
+    if (elapsed > duration) {
+      if (typeof callback === 'function') {
+        callback(el);
+      }
     } else {
-      var end = getTop(el);
+      requestAnimationFrame(step);
     }
-
-    var clock = Date.now();
-    var requestAnimationFrame = window.requestAnimationFrame ||
-        window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame ||
-        function(fn){window.setTimeout(fn, 15);};
-
-    var step = function(){
-        var elapsed = Date.now() - clock;
-        if (context !== window) {
-        	context.scrollTop = position(start, end, elapsed, duration);
-        }
-        else {
-        	window.scroll(0, position(start, end, elapsed, duration));
-        }
-
-        if (elapsed > duration) {
-            if (typeof callback === 'function') {
-                callback(el);
-            }
-        } else {
-            requestAnimationFrame(step);
-        }
-    }
-    step();
-}
+  };
+  step();
+};
 
 export default smoothScroll;
