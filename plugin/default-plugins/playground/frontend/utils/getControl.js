dd7523b861426722f8ef3473a252eeca5ad7d22e
Nik Graf <nik@nikgraf.com>
2016-05-21 22:52:28 +0200

    setup new controls setting structure

--- plugin/default-plugins/playground/frontend/utils/getControl.js
+++ plugin/default-plugins/playground/frontend/utils/getControl.js
@@ -6,6 +6,7 @@
  */
 
 import React from 'react';
+import has from 'lodash/has';
 import BooleanControl from '../components/controls/base/BooleanControl';
 import IntegerControl from '../components/controls/base/IntegerControl';
 import ArrayControl from '../components/controls/base/ArrayControl';
@@ -18,55 +19,69 @@ import ObjectControl from '../components/controls/base/ObjectControl';
 import FlowArrayControl from '../components/controls/base/FlowArrayControl';
 import FlowUnionControl from '../components/controls/base/FlowUnionControl';
 
+const defaultControls = {
+  bool: {
+    control: BooleanControl,
+    nested: false,
+  },
+  boolean: {
+    control: BooleanControl,
+    nested: false,
+  },
+  number: {
+    control: IntegerControl,
+    nested: false,
+  },
+  string: {
+    control: StringControl,
+    nested: false,
+  },
+  node: {
+    control: NodeControl,
+    nested: false,
+  },
+  enum: {
+    control: EnumControl,
+    nested: true,
+  },
+  arrayOf: {
+    control: ArrayControl,
+    nested: true,
+  },
+  Array: {
+    control: FlowArrayControl,
+    nested: true,
+  },
+  shape: {
+    control: ObjectControl,
+    nested: true,
+  },
+  signature: {
+    control: FlowObjectControl,
+    nested: true,
+  },
+  union: {
+    control: FlowUnionControl,
+    nested: true,
+  },
+};
+
 const getControl = (propType, propMeta) => {
   const controlType = propMeta && propMeta.controlType ? propMeta.controlType : propType.name;
   const constraints = propMeta && propMeta.constraints ? propMeta.constraints : {};
-  const generalProps = {
+  const controlProps = {
     // If something is required, don't randomise it to null/undefined
     required: propType.required,
     constraints,
   };
 
-  let control;
-  switch (controlType) {
-    case 'bool': // proptypes boolean
-      control = <BooleanControl {...generalProps} />;
-      break;
-    case 'boolean': // flow boolean
-      control = <BooleanControl {...generalProps} />;
-      break;
-    case 'number':
-      control = <IntegerControl {...generalProps} />;
-      break;
-    case 'string':
-      control = <StringControl {...generalProps} />;
-      break;
-    case 'enum':
-      control = <EnumControl propTypeData={propType} {...generalProps} />;
-      break;
-    case 'node':
-      control = <NodeControl {...generalProps} />;
-      break;
-    case 'shape': // proptypes
-      control = <ObjectControl propTypeData={propType} {...generalProps} />;
-      break;
-    case 'signature': // flow
-      control = <FlowObjectControl propTypeData={propType} {...generalProps} />;
-      break;
-    case 'arrayOf':
-      control = <ArrayControl propTypeData={propType} {...generalProps} />;
-      break;
-    case 'Array':
-      control = <FlowArrayControl propTypeData={propType} {...generalProps} />;
-      break;
-    case 'union':
-      control = <FlowUnionControl propTypeData={propType} {...generalProps} />;
-      break;
-    default:
-      control = <DummyControl />;
-      break;
+  if (has(defaultControls, controlType)) {
+    const Control = defaultControls[controlType].control;
+    controlProps.propTypeData = propType;
+    return <Control {...controlProps} />;
   }
-  return control;
+
+  return <DummyControl />;
 };
 
 export default getControl;
