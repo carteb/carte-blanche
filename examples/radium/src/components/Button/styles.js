cf6d16f2aea802509491f258c5a1f908bf671618
Max <contact@mstoiber.com>
2016-05-22 10:53:04 +0200

    Add Radium styling example

---
+++ examples/radium/src/components/Button/styles.js
@@ -0,0 +1,17 @@
+const styles = {
+  button: {
+    padding: '0.5em 1.75em',
+    fontSize: '0.75em',
+    borderRadius: '3px',
+    border: '1px solid #6CC0E5',
+    color: '#6CC0E5',
+    backgroundColor: 'white',
+  },
+  primary: {
+    backgroundColor: '#6CC0E5',
+    color: 'white',
+    border: '1px solid #6CC0E5',
+  },
+};
+
+export default styles;
