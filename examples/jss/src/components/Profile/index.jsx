c54ff9a2aea55e0e4bfeb51f9b61e130c3e21cc5
Max <contact@mstoiber.com>
2016-05-22 11:21:25 +0200

    Add JSS example

---
+++ examples/jss/src/components/Profile/index.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import useSheet from 'react-jss';
+import jss from 'jss';
+import extend from 'jss-extend';
+
+// Setup jss plugins.
+jss.use(extend());
+import styles from './styles';
+
+import Button from '../Button/index.jsx';
+
+class Profile extends Component {
+  static propTypes = {
+    avatarUrl: React.PropTypes.string.isRequired,
+    firstName: React.PropTypes.string.isRequired,
+    lastName: React.PropTypes.string,
+    username: React.PropTypes.string.isRequired,
+    bio: React.PropTypes.string,
+  };
+
+  onAddFriend = () => {
+    alert(`Add @${this.props.username} as a friend`);
+  };
+
+  render() {
+    return (
+      <div className={this.props.sheet.classes.card}>
+        <div className={this.props.sheet.classes.row}>
+          <img
+            className={this.props.sheet.classes.avatar}
+            src={this.props.avatarUrl}
+            alt={`${this.props.firstName} ${this.props.lastName}`}
+          />
+          <div className={this.props.sheet.classes.information}>
+            <h1 className={this.props.sheet.classes.name}>
+              {this.props.firstName}{(this.props.lastName) ? (` ${this.props.lastName}`) : null}
+            </h1>
+            <h2 className={this.props.sheet.classes.username}>@{this.props.username}</h2>
+          </div>
+        </div>
+        <p className={this.props.sheet.classes.paragraph}>{this.props.bio}</p>
+        <div className={this.props.sheet.classes.buttonWrapper}>
+          <Button type="secondary" onClick={this.onAddFriend}>
+            Add friend!
+          </Button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default useSheet(Profile, styles);
---
+++ examples/jss/src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Profile from './components/Profile/index.jsx';
+import styles from './styles';
+
+ReactDOM.render(
+  <div style={styles.wrapper}>
+    <style>{`
+      html,
+      body,
+      #root {
+        width: 100%;
+        height: 100%;
+        background-color: #f4f7f9;
+        overflow: hidden;
+        margin: 0;
+        padding: 0;
+      }
+    `}</style>
+    <Profile
+      avatarUrl="https://pbs.twimg.com/profile_images/681114454029942784/PwhopfmU.jpg"
+      firstName="Max"
+      lastName="Stoiber"
+      username="mxstbr"
+      bio="I travel around the world, brew coffee, ski mountains and make stuff on the web. Open source developer advocate (@KeystoneJS @ElementalUI), part of @reactvienna." // eslint-disable-line max-len
+    />
+  </div>,
+  document.getElementById('root')
+);
