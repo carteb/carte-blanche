92ba6362b02c5558d547ba97eb01d0586fe97cb7
Max <contact@mstoiber.com>
2016-05-28 12:53:00 +0200

    Put Aphrodite Example in line with other examples

--- examples/aphrodite/src/components/Button/index.js
+++ examples/aphrodite/src/components/Button/index.js
@@ -1,26 +1,7 @@
 import React, { PropTypes } from 'react';
-import { StyleSheet, css } from 'aphrodite';
+import { css } from 'aphrodite';
 
-const baseStyles = {
-  padding: '0.5em 1.75em',
-  fontSize: '0.75em',
-  borderRadius: '3px',
-};
-
-const styles = StyleSheet.create({
-  button: {
-    ...baseStyles,
-    border: '1px solid #6CC0E5',
-    color: '#6CC0E5',
-    backgroundColor: 'white',
-  },
-  buttonPrimary: {
-    ...baseStyles,
-    backgroundColor: '#6CC0E5',
-    color: 'white',
-    border: '1px solid #6CC0E5',
-  },
-});
+import styles from './styles';
 
 const Button = (props) => (
   <button
