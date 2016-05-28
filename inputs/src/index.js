7e415e3f47ddcd878edca908305273e62d9126e6
Nik Graf <nik@nikgraf.com>
2016-05-29 00:26:48 +0200

    first draft of the inputs

---
+++ inputs/src/index.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import AtriumInput from './components/AtriumInput';
+import styles from './styles.css';
+
+class App extends React.Component {
+
+  state = {
+    firstValue: 'some String',
+    secondValue: 'another String',
+    thirdValue: 'another String',
+    fourthValue: undefined,
+  }
+
+  onChangeFirstValue = ({ value }) => {
+    this.setState({
+      firstValue: value,
+    });
+  };
+
+  onChangeSecondValue = ({ value }) => {
+    this.setState({
+      secondValue: value,
+    });
+  };
+
+  onChangeThirdValue = ({ value }) => {
+    this.setState({
+      thirdValue: value,
+    });
+  };
+
+  onChangeFourthValue = ({ value }) => {
+    this.setState({
+      fourthValue: value,
+    });
+  };
+
+  render() {
+    return (
+      <div className={styles.root}>
+        <AtriumInput
+          value={this.state.firstValue}
+          fallbackValue=""
+          onChange={this.onChangeFirstValue}
+        />
+        <AtriumInput
+          value={this.state.secondValue}
+          fallbackValue=""
+          onChange={this.onChangeSecondValue}
+          hasSettings
+        />
+        <AtriumInput
+          value={this.state.thirdValue}
+          fallbackValue=""
+          onChange={this.onChangeThirdValue}
+          hasRandomButton
+        />
+        <AtriumInput
+          value={this.state.fourthValue}
+          fallbackValue=""
+          onChange={this.onChangeFourthValue}
+          hasRandomButton
+          hasSettings
+        />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
