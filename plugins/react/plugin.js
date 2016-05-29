29d65c631c3d30f100cff03b4e7ad0fee73bc047
Max <contact@mstoiber.com>
2016-05-29 17:21:37 +0200

    More and better validation from ReactPlugin

--- plugins/react/plugin.js
+++ plugins/react/plugin.js
@@ -7,21 +7,36 @@ const defaults = require('lodash/defaults');
 const styleguideResolver = require('./resolver.js').default;
 
 function ReactPlugin(options) {
+  // Make sure the plugin was instantiated as a constructor, i.e. new ReactPlugin()
+  if (!(this instanceof ReactPlugin)) {
+    throw new Error(
+      'The ReactPlugin must be instantiated with the "new" keyword, i.e. "new ReactPlugin()"\n\n'
+    );
+  }
+
   this.options = options || {};
 
   // The hostname option must be a string
   if (this.options.hostname && !isString(this.options.hostname)) {
-    throw new Error('The "hostname" option of the ReactPlugin must be a String!\n\n');
+    throw new Error('The "hostname" option of the ReactPlugin must be a string!\n\n');
   }
 
+  const parsedPort = parseFloat(this.options.port);
   // The port option must be something that can be made a number
-  if (this.options.port && !isNaN(parseFloat(this.options.port))) {
-    throw new Error('The "port" option of the ReactPlugin must be a Number!\n\n');
+  if (this.options.port && !isNaN(parsedPort)) {
+    throw new Error('The "port" option of the ReactPlugin must be a number!\n\n');
+  }
+
+  // If the port can be interpreted as a number, it must be above 0 and below 65535
+  if (parsedPort < 0 || parsedPort > 65535) {
+    throw new Error('The "port" must be between 0 and 65535 (inc)!\n\n');
   }
 
   // The variationFolderName option must be a string
   if (this.options.variationFolderName && !isString(this.options.variationFolderName)) {
-    throw new Error('The "variationFolderName" option of the ReactPlugin must be a String!\n\n'); // eslint-disable-line max-len
+    throw new Error(
+      'The "variationFolderName" option of the ReactPlugin must be a string!\n\n'
+    );
   }
 }
 /**
