28ae0682c879ca8f232d4025967d8f3c37bfbd16
Nik Graf <nik@nikgraf.com>
2016-05-30 11:41:49 +0200

    style object control

--- plugins/react/frontend/components/controls/base/ObjectControl/index.js
+++ plugins/react/frontend/components/controls/base/ObjectControl/index.js
@@ -8,46 +8,77 @@ import renderControls from '../../../../utils/renderControls';
 import getControl from '../../../../utils/getControl';
 import React from 'react';
 import mapValues from 'lodash/mapValues';
-import Label from '../../../common/Label';
 import randomValue from './randomValue';
-import styles from './styles.css';
 import ConstraintsForm from './ConstraintsForm';
+import Row from '../../../form/Grid/Row';
+import LeftColumn from '../../../form/Grid/LeftColumn';
+import RightColumn from '../../../form/Grid/RightColumn';
+import Label from '../../../form/Label';
+import RandomButton from '../../../form/RandomButton';
+import SettingsButton from '../../../form/SettingsButton';
+import BaseSettings from '../../../form/BaseSettings';
+import Dropdown from '../../../form/Dropdown';
 
-const ObjectControl = ({ label, propTypeData, value, onUpdate, isNested }) => {
-  const updatePropertyValues = (values) => {
-    onUpdate({ value: values });
-  };
+class ObjectControl extends React.Component {
 
-  const normalizedPropsWithControls = mapValues(propTypeData.value, (prop) => {
-    prop.control = getControl(prop); // eslint-disable-line no-param-reassign
-    return prop;
-  });
-
-  let controlWrapperClassName = styles.nestedControls;
-  if (!label && isNested) {
-    controlWrapperClassName = styles['nestedDeeperThanOneLevel--without-label'];
-  } else if (isNested) {
-    controlWrapperClassName = styles.nestedDeeperThanOneLevel;
+  state = {
+    settingsActive: false,
   }
 
-  return (
-    <div className={styles.wrapper}>
-      {/* inside arrays there is no label for the object */}
-      {(label) && (
-        <Label
-          text={label}
-          isNested={isNested}
-          onRandomClick={() => onUpdate({
-            value: ObjectControl.randomValue(propTypeData),
-          })}
-        />
-      )}
-      <div className={controlWrapperClassName}>
-        {renderControls(normalizedPropsWithControls, value, updatePropertyValues, true)}
-      </div>
-    </div>
-  );
-};
+  onToggleSettings = () => {
+    this.setState({
+      settingsActive: !this.state.settingsActive,
+    });
+  }
+
+  render() {
+    const { label, secondaryLabel, propTypeData, value, onUpdate, isNested } = this.props;
+
+    const hasSettings = true;
+
+    const updatePropertyValues = (values) => {
+      onUpdate({ value: values });
+    };
+
+    const normalizedPropsWithControls = mapValues(propTypeData.value, (prop) => {
+      prop.control = getControl(prop); // eslint-disable-line no-param-reassign
+      return prop;
+    });
+
+    return (
+      <Row>
+        <LeftColumn>
+          {/* inside arrays there is no label for the object */}
+          {(label) && (
+            <Label
+              type={secondaryLabel}
+              propKey={label}
+            />
+          )}
+        </LeftColumn>
+        <RightColumn>
+          <div style={{ padding: '0 0.5em', textAlign: 'right' }}>
+            {hasSettings && <Dropdown
+              active={this.state.settingsActive}
+            >
+              <BaseSettings onChange={onUpdate} />
+            </Dropdown>}
+            <SettingsButton
+              groupType="left"
+              onClick={this.onToggleSettings}
+            />
+            <RandomButton
+              onClick={() => onUpdate({ value: ObjectControl.randomValue(propTypeData) })}
+            />
+          </div>
+        </RightColumn>
+        <Row>
+          {renderControls(normalizedPropsWithControls, value, updatePropertyValues, true)}
+        </Row>
+      </Row>
+    );
+  }
+}
 
 ObjectControl.randomValue = randomValue;
 
