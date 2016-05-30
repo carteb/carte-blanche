f7e3c465d0c19c786c7dfe613054ce25f386777b
Jan Nicklas <j.nicklas@me.com>
2016-05-30 09:43:02 +0200

    use relative paths as component key

--- webpack-plugin/dynamic-resolve.js
+++ webpack-plugin/dynamic-resolve.js
@@ -1,12 +1,14 @@
 /* eslint-disable max-len */
 import loaderUtils from 'loader-utils';
+import path from 'path';
 
 module.exports = function dynamicResolve() {
   this.cacheable();
   const query = loaderUtils.parseQuery(this.query);
   const filter = query.filter;
-  const componentRoot = query.componentRoot;
-  const context = query.context;
+  // TODO check on windows
+  const absoluteComponentRoot = path.resolve(query.context, query.componentRoot);
+  const relativeComponentRoot = path.relative(query.context, absoluteComponentRoot);
 
   const loaderMapping = {
     compiledComponent: '',
@@ -18,7 +20,7 @@ module.exports = function dynamicResolve() {
 
   // Add dynamics requires for every loaders
   loaders.forEach((loader) => {
-    loaderMapping[loader] = `require.context('${loaderMapping[loader]}${context}/${componentRoot}', true, ${filter})`;
+    loaderMapping[loader] = `require.context('${loaderMapping[loader]}${absoluteComponentRoot}', true, ${filter})`;
   });
 
   return `
@@ -29,12 +31,13 @@ module.exports = function dynamicResolve() {
     // Gather all loader information for every loader
     var components = {};
     resources['${loaders[0]}'].keys().forEach(function(componentFile) {
-     components[componentFile] = {};
-     Object.keys(resources).forEach(function(loader) {
-       components[componentFile]['get' + loader.substr(0, 1).toUpperCase() + loader.substr(1)] = function() {
-         return resources[loader](componentFile);
-       };
-     });
+      componentFile = componentFile.replace(/\.\\//, '${relativeComponentRoot}/');
+      components[componentFile] = {};
+      Object.keys(resources).forEach(function(loader) {
+        components[componentFile]['get' + loader.substr(0, 1).toUpperCase() + loader.substr(1)] = function() {
+          return resources[loader](componentFile);
+        };
+      });
     });
     window.$INITIALIZE_COMPONENT_GUI(components);
    `;
