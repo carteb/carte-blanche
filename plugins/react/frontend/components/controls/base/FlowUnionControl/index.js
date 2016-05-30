e88be86921dd78c456dda142f3bf26a981dafc9b
Nik Graf <nik@nikgraf.com>
2016-05-30 14:09:32 +0200

    style flow union control

--- plugins/react/frontend/components/controls/base/FlowUnionControl/index.js
+++ plugins/react/frontend/components/controls/base/FlowUnionControl/index.js
@@ -6,50 +6,67 @@
 
 import React, { cloneElement } from 'react';
 import getControl from '../../../../utils/getControl';
-import Label from '../../../common/Label';
 import randomValue from './randomValue';
+import Row from '../../../form/Grid/Row';
+import LeftColumn from '../../../form/Grid/LeftColumn';
+import RightColumn from '../../../form/Grid/RightColumn';
+import Label from '../../../form/Label';
 
-const FlowUnionControl = ({ label, propTypeData, value, onUpdate }) => {
+const FlowUnionControl = (props) => {
+  const {
+    label,
+    propTypeData,
+    value,
+    onUpdate,
+    nestedLevel,
+    secondaryLabel,
+  } = props;
   let clonedControl;
   if (typeof value === 'undefined' || value === null) {
     clonedControl = `${value}`;
   } else {
     const currentControl = getControl({ name: (typeof value) });
-    const props = {
+    const subControlProps = {
       label: undefined, // TODO cleanup
       value,
       onUpdate: (data) => onUpdate({ value: data.value }),
     };
-    clonedControl = cloneElement(currentControl, props);
+    clonedControl = cloneElement(currentControl, subControlProps);
   }
 
   return (
-    <div>
-      <Label
-        text={label}
-        onRandomClick={() => onUpdate({
-          value: FlowUnionControl.randomValue(propTypeData),
-        })}
-      />
-      <select
-        value={typeof value}
-        onChange={(event) => {
-          const newTypeName = event.target.value;
-          const control = getControl({ name: newTypeName });
-          return onUpdate({ value: control.type.randomValue({}) });
-        }}
-      >
-        {propTypeData.elements.map((type, index) => (
-          <option
-            value={type.name}
-            key={index}
+    <Row>
+      <LeftColumn nestedLevel={nestedLevel}>
+        <Label
+          type={secondaryLabel}
+          propKey={label}
+        />
+      </LeftColumn>
+      <RightColumn>
+        <div style={{ float: 'right' }}>
+          <select
+            value={typeof value}
+            onChange={(event) => {
+              const newTypeName = event.target.value;
+              const control = getControl({ name: newTypeName });
+              return onUpdate({ value: control.type.randomValue({}) });
+            }}
           >
-            {type.name}
-          </option>
-        ))}
-      </select>
-      {clonedControl}
-    </div>
+            {propTypeData.elements.map((type, index) => (
+              <option
+                value={type.name}
+                key={index}
+              >
+                {type.name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div>
+          {clonedControl}
+        </div>
+      </RightColumn>
+    </Row>
   );
 };
 
