2382b5059295cfdf5d58977c40180c6c35d813ce
Max <contact@mstoiber.com>
2016-05-30 13:08:16 +0200

    Fix default plugins

--- webpack-plugin/styleguide-plugin.js
+++ webpack-plugin/styleguide-plugin.js
@@ -92,9 +92,19 @@ StyleguidePlugin.prototype.apply = function apply(compiler) {
  * Register the default plugins
  */
 StyleguidePlugin.prototype.registerDefaultPlugins = function registerDefaultPlugins(compiler) {
-  const ReactPlugin = require('atrium-react-plugin-beta'); // eslint-disable-line global-require, import/no-unresolved, max-len
-  const reactPlugin = new ReactPlugin();
-  reactPlugin.apply(compiler);
+  let ReactPlugin = require('atrium-react-plugin-beta'); // eslint-disable-line global-require, import/no-unresolved, max-len
+  try {
+    const reactPlugin = new ReactPlugin();
+    reactPlugin.apply(compiler);
+  } catch (err) {
+    try {
+      ReactPlugin = require('../plugins/react/plugin'); // eslint-disable-line global-require
+      const reactPlugin = new ReactPlugin();
+      reactPlugin.apply(compiler);
+    } catch (ex) {
+      console.log('ERROR Installing default Styleguide plugins failed.'); // eslint-disable-line no-console,max-len
+    }
+  }
 };
 
 /**
