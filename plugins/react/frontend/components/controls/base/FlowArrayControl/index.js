c0854b8f579f5d4767c50d4185abdc30217f046d
Nik Graf <nik@nikgraf.com>
2016-05-30 14:26:46 +0200

    style flow array control

--- plugins/react/frontend/components/controls/base/FlowArrayControl/index.js
+++ plugins/react/frontend/components/controls/base/FlowArrayControl/index.js
@@ -2,49 +2,98 @@ import React, { cloneElement } from 'react';
 import cloneDeep from 'lodash/cloneDeep';
 import range from 'lodash/range';
 import randomValue from './randomValue';
-import Label from '../../../common/Label';
+import first from 'lodash/first';
 import getControl from '../../../../utils/getControl';
-import objectControlStyles from '../ObjectControl/styles.css';
+import Row from '../../../form/Grid/Row';
+import LeftColumn from '../../../form/Grid/LeftColumn';
+import RightColumn from '../../../form/Grid/RightColumn';
+import Label from '../../../form/Label';
+import Button from '../../../form/Button';
+import RandomButton from '../../../form/RandomButton';
+import SettingsButton from '../../../form/SettingsButton';
+import BaseSettings from '../../../form/BaseSettings';
+import Dropdown from '../../../form/Dropdown';
+import isUndefined from 'lodash/isUndefined';
+import isNull from 'lodash/isNull';
 
 
-const FlowArrayControl = (props) => {
-  const {
-    label,
-    onUpdate,
-    value,
-    propTypeData,
-    nestedLevel,
-  } = props;
+class FlowArrayControl extends React.Component {
 
-  const size = props.value === null || typeof props.value === 'undefined' ? 0 : props.value.length;
-  const rangeArray = range(size);
+  state = {
+    settingsActive: false,
+  }
 
-  const onUpdateEntry = (data, index) => {
-    const newValue = cloneDeep(value);
-    newValue[index] = data;
-    onUpdate({ value: newValue });
-  };
+  onToggleSettings = () => {
+    this.setState({
+      settingsActive: !this.state.settingsActive,
+    });
+  }
 
-  // TODO fix this for multiples ones
-  const control = getControl(propTypeData.elements[0]);
+  render() {
+    const {
+      label,
+      onUpdate,
+      value,
+      propTypeData,
+      nestedLevel,
+      secondaryLabel,
+    } = this.props;
 
-  return (
-    <div className={objectControlStyles.wrapper}>
-      <Label
-        text={label}
-        onRandomClick={() => onUpdate({ value: FlowArrayControl.randomValue(propTypeData) })}
-      />
-      {(size !== 0) && (
-        <div
-          className={
-            (nestedLevel > 0) ?
-            objectControlStyles.nestedControls :
-            objectControlStyles.wrapper
-          }
-          style={{
-            paddingLeft: '1rem',
-          }}
-        >
+    const hasSettings = true;
+    const size = isNull(value) || isUndefined(value) ? 0 : this.props.value.length;
+    const rangeArray = range(size);
+
+    const onUpdateEntry = (data, index) => {
+      const newValue = cloneDeep(value);
+      newValue[index] = data;
+      onUpdate({ value: newValue });
+    };
+
+    const requiredPropTypeData = { ...propTypeData, required: true };
+    const addItem = () => ({ value: [...value || [], first(FlowArrayControl.randomValue(requiredPropTypeData))] }); // eslint-disable-line max-len
+    const removeItem = () => ({ value: size > 0 ? [...value.slice(0, size - 1)] : [] });
+
+    // TODO fix this for multiples ones
+    const control = getControl(propTypeData.elements[0]);
+
+    return (
+      <Row>
+        <LeftColumn nestedLevel={nestedLevel}>
+          <Label
+            type={secondaryLabel}
+            propKey={label}
+          />
+        </LeftColumn>
+        <RightColumn>
+          <div style={{ padding: '0 0.5rem', textAlign: 'right' }}>
+            {hasSettings && <Dropdown
+              active={this.state.settingsActive}
+            >
+              <BaseSettings onChange={onUpdate} />
+            </Dropdown>}
+            <SettingsButton
+              groupType={'left'}
+              onClick={this.onToggleSettings}
+            />
+            <Button
+              groupType="center"
+              onClick={() => onUpdate(removeItem())}
+              disabled={size <= 0}
+            >
+              -
+            </Button>
+            <Button
+              onClick={() => onUpdate(addItem())}
+              groupType="center"
+            >
+              +
+            </Button>
+            <RandomButton
+              onClick={() => onUpdate({ value: FlowArrayControl.randomValue(propTypeData) })}
+            />
+          </div>
+        </RightColumn>
+        <Row>
           {rangeArray && rangeArray.map((index) => {
             const newProps = {
               key: index,
@@ -54,11 +103,11 @@ const FlowArrayControl = (props) => {
             };
             return cloneElement(control, newProps);
           })}
-        </div>
-      )}
-    </div>
-  );
-};
+        </Row>
+      </Row>
+    );
+  }
+}
 
 FlowArrayControl.randomValue = randomValue;
 
