4fff2fb85c1328a2e2b56eedc647dfd89c538b7d
Nik Graf <nik@nikgraf.com>
2016-06-01 18:34:02 +0200

    fix dropdown behaviour

--- plugins/react/frontend/components/form/Dropdown/index.js
+++ plugins/react/frontend/components/form/Dropdown/index.js
@@ -2,8 +2,42 @@ import React from 'react';
 
 import styles from './styles.css';
 
-const Dropdown = ({ active = false, ...otherProps }) => (
-  <div {...otherProps} className={active ? styles.root : styles.hidden} />
-);
+class Dropdown extends React.Component {
+
+  componentDidMount() {
+    this.ignoreNextClose = false;
+    document.addEventListener('click', this.onClose, false);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.ignoreNextClose = nextProps.active && !this.props.active;
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('click', this.onClose, false);
+  }
+
+  onClose = (evt) => {
+    if (this.props.onClose &&
+        this.props.active &&
+        !this.ignoreNextClose &&
+        !this.wrapper.contains(evt.target)
+    ) {
+      this.props.onClose();
+    }
+    this.ignoreNextClose = false;
+  }
+
+  render() {
+    const { active = false, ...otherProps } = this.props;
+    return (
+      <div
+        {...otherProps}
+        className={active ? styles.root : styles.hidden}
+        ref={(ref) => { this.wrapper = ref; }}
+      />
+    );
+  }
+}
 
 export default Dropdown;
