7c0413482538ae4119fd166f04f7fa154f6c625f
Max Stoiber <contact@mxstbr.com>
2016-06-13 20:31:46 +0200

    (core) Support libraries (#345)
    * Add support for libraries
    * Add library example

---
+++ examples/library/webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const webpack = require('webpack');
+
+// To use the dev version, rename this file to webpack.config.babel.js (the .babel is missing atm)
+const CarteBlanche = require('../../webpack-plugin/dist/index');
+
+const commonEntrypoints = [
+  'react-hot-loader/patch',
+  'webpack-dev-server/client?http://localhost:8080',
+  'webpack/hot/only-dev-server',
+];
+// bundle currently only contains phonesystem stuff hence this name
+const entrypoints = {
+  main: [...commonEntrypoints, './src/index'],
+  foobar: [...commonEntrypoints, './src/index.foobar'],
+};
+
+module.exports = {
+  devtool: 'eval',
+  entry: entrypoints,
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js',
+    publicPath: 'http://localhost:8080/',
+    libraryTarget: 'var',
+    library: '[name]',
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new CarteBlanche({ componentRoot: './src' }),
+  ],
+  devServer: {
+    contentBase: './dist',
+    compress: true,
+    hot: true,
+    historyApiFallback: true,
+  },
+  resolveLoader: {
+    fallback: path.join(__dirname, 'node_modules'),
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      loader: 'babel',
+      query: {
+        presets: ['es2015', 'react', 'stage-1'],
+      },
+      exclude: /node_modules/,
+      include: [path.join(__dirname, 'src')],
+    },
+    ],
+  },
+};
