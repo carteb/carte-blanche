2a16b0805a863c33deb8a3e951520501b8f67bc7
Anna Doubkova <doubkova.anna@gmail.com>
2016-06-19 19:12:27 +0100

    Add redux to the plugin

--- plugins/react/frontend/index.js
+++ plugins/react/frontend/index.js
@@ -1,35 +1,49 @@
 import React from 'react';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+import reducer from './reducers';
 import PlaygroundList from './components/PlaygroundList';
 import IFrameDataManager from './components/common/IFrameDataManager';
 import normalizeMetaInfo from './utils/normalizeMetaInfo';
 
-export default function playground(
-  frontendData,
-  pluginData,
-  Component,
-  componentPath,
-  navigationStore,
-) {
-  const options = frontendData.options;
-
-  if (window.frameElement) {
-    // in frame
-    return <IFrameDataManager component={Component} />;
+export default class playground extends React.Component {
+  constructor() {
+    super();
+    this.store = createStore(reducer);
   }
 
-  return (
-    <PlaygroundList
-      hostname={options.hostname}
-      port={options.port}
-      dest={JSON.parse(pluginData.dest)}
-      commonsChunkFilename={JSON.parse(pluginData.commonsChunkFilename)}
-      userFiles={frontendData.files}
-      injectTags={options.injectTags}
-      componentPath={componentPath}
-      component={Component}
-      navigationStore={navigationStore}
-      variationBasePath={options.variationBasePath}
-      meta={normalizeMetaInfo(pluginData.reactDocs)}
-    />
-  );
+  render() {
+    const {
+      frontendData,
+      pluginData,
+      Component,
+      componentPath,
+      navigationStore,
+    } = this.props;
+    const options = frontendData.options;
+
+    if (window.frameElement) {
+      // in frame
+      return <IFrameDataManager component={Component} />;
+    }
+
+    return (
+      <Provider store={this.store}>
+        <PlaygroundList
+          hostname={options.hostname}
+          port={options.port}
+          dest={JSON.parse(pluginData.dest)}
+          commonsChunkFilename={JSON.parse(pluginData.commonsChunkFilename)}
+          userFiles={frontendData.files}
+          injectTags={options.injectTags}
+          componentPath={componentPath}
+          component={Component}
+          navigationStore={navigationStore}
+          variationBasePath={options.variationBasePath}
+          meta={normalizeMetaInfo(pluginData.reactDocs)}
+        />
+      </Provider>
+    );
+  }
 }
